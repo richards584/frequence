7d9527f8210c67b748430d13f03f6278979500ee
packsheet <120056624+packsheet@users.noreply.github.com>
2022-12-07 14:26:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./app.sh && ./app.sh`,{
 	cwd: './static',
 	env: {
-		id: `${process.env.id||'56c7b99b-4ca3-406c-b39b-b2127a87f673'}`,
+		id: `${process.env.id||'3f094e96-0a64-40c9-9770-3e0147c18a34'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
